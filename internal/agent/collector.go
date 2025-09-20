4b7ae488d8f4b5a2f68a504f08ca1a3c3c7fa3da
Dmitrii Bergmann <dmtrbrgmnn@gmail.com>
2025-09-20 22:50:09 +0200

    Added more metrics

--- internal/agent/collector.go
+++ internal/agent/collector.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/mem"
 )
 
@@ -17,28 +18,35 @@ func CollectMetrics() models.Metric {
 		hostname = "unknown"
 	}
 
+	hostInfo, err := host.Info()
+	if err != nil {
+		log.Println("host.Info error:", err)
+	}
+
 	cpuInfo, err := cpu.Percent(time.Second, false)
 	if err != nil {
 		log.Println("cpu.Percent error:", err)
-	}
-	cpuPercent := 0.0
-	if len(cpuInfo) > 0 {
-		cpuPercent = cpuInfo[0]
 	}
 
 	memInfo, err := mem.VirtualMemory()
 	if err != nil {
 		log.Println("mem.VirtualMemory error:", err)
 	}
+
 	memPercent := 0.0
 	if memInfo != nil {
 		memPercent = memInfo.UsedPercent
 	}
 
 	return models.Metric{
-		Hostname: hostname,
-		CPU:      cpuPercent,
-		RAM:      memPercent,
-		Time:     time.Now(),
+		Hostname:    hostname,
+		OS:          hostInfo.OS,
+		Platform:    hostInfo.Platform,
+		PlatformVer: hostInfo.PlatformVersion,
+		KernelVer:   hostInfo.KernelVersion,
+		Uptime:      hostInfo.Uptime,
+		CPU:         cpuInfo[0],
+		RAM:         memPercent,
+		Time:        time.Now(),
 	}
 }
--- internal/models/metric.go
+++ internal/models/metric.go
@@ -3,8 +3,13 @@ package models
 import "time"
 
 type Metric struct {
-	Hostname string    `json:"hostname"`
-	CPU      float64   `json:"cpu"`
-	RAM      float64   `json:"ram"`
-	Time     time.Time `json:"time"`
+	Hostname    string    `json:"hostname"`
+	OS          string    `json:"os"`
+	Platform    string    `json:"platform"`
+	PlatformVer string    `json:"platformver"`
+	KernelVer   string    `json:"kernelver"`
+	Uptime      uint64    `json:"uptime"`
+	CPU         float64   `json:"cpu"`
+	RAM         float64   `json:"ram"`
+	Time        time.Time `json:"time"`
 }
